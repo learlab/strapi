6ebb9fd6e5c400b4775dbaf8f91342194b2dc1c3
theannachen <annachen031014@gmail.com>
2023-10-17 18:09:55 -0500

    please

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -5,7 +5,11 @@ export default ({ env }) => ({
     keys: env.array('APP_KEYS'),
   },
   admin:{
-    host: "https://strapi-pi-eight.vercel.app/",
+    "path": "/",
+    "serveAdminPanel": false, // http://yourbackend.com will not serve any static admin files
+    "build": {
+      "backend": " https://strapi-pi-eight.vercel.app/\n"
+    }
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
