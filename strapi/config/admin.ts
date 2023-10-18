5d70c5095414796d56c47abf84053d5721b6fc70
theannachen <annachen031014@gmail.com>
2023-10-18 00:46:17 -0500

    added possible front end

--- strapi/config/admin.ts
+++ strapi/config/admin.ts
@@ -1,4 +1,6 @@
 export default ({ env }) => ({
+  host: "0.0.0.0", // only used along with `strapi develop --watch-admin` command
+  port: 3000, // only used along with `strapi develop --watch-admin` command
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
