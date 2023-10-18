c13d9a3eab034f7276494b198a2fc24834fd6664
theannachen <annachen031014@gmail.com>
2023-10-18 02:30:25 -0500

    env var

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://strapi-pi-eight.vercel.app',
+  url: 'SERVER_URL',
   app: {
     keys: env.array('APP_KEYS'),
   },
