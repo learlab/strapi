2c300aa52c48fb33ff4765234c907de85b249ade
theannachen <annachen031014@gmail.com>
2023-10-18 01:52:07 -0500

    url root

--- strapi/config/admin.ts
+++ strapi/config/admin.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: "0.0.0.0", // only used along with `strapi develop --watch-admin` command
   port: 3000, // only used along with `strapi develop --watch-admin` command
+  url: '/',
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
