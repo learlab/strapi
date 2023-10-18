b5f172b9c19c1eb965367275db097f0452165964
theannachen <annachen031014@gmail.com>
2023-10-18 14:32:47 -0500

    testing ports

--- strapi/config/server.ts
+++ strapi/config/server.ts
@@ -1,16 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'SERVER_URL',
   app: {
     keys: env.array('APP_KEYS'),
-  },
-  admin:{
-    "path": "/",
-    "serveAdminPanel": false, // http://yourbackend.com will not serve any static admin files
-    "build": {
-      "backend": " https://strapi-pi-eight.vercel.app/\n"
-    }
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
