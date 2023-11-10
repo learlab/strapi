9d9f982507fc4095887ac9ca5bb97cf4d9c27f89
langdonholmes <55119338+langdonholmes@users.noreply.github.com>
2023-11-10 22:47:37 +0000

    beforeCreate for videos

--- src/extensions/videoLifecycle.js
+++ src/extensions/videoLifecycle.js
@@ -7,40 +7,61 @@ module.exports = ({ strapi }) => {
     _metadata.set("page.video", {
         ..._metadata.get("page.video"),
         lifecycles: {
-            beforeUpdate: async (event) => {
+            beforeCreate: async (event) => {
                 // incoming data update
-                const { data, where } = event.params;
+                const { data } = event.params;
 
-                // omit fields that are not included in oldData
-                var newData = _.omit(data, ["__component", "__temp_key__", "updatedAt"]);
-                // console.log(newData);
+                const transcript = await strapi
+                    .service('plugin::auto-content.fetchTranscriptService')
+                    .getTranscript(data.URL, data.StartTime, data.EndTime)
 
-                // get the old data from the database
-                const oldData = await strapi
-                    .query("page.video")
-                    .findOne({ where: { id: where.id } });
+                var mdx = '<YoutubeVideo\n' +
+                    `src=${data.URL}\n`
 
-                // console.log(oldData);
+                if (data.Title) mdx += `title="${data.Title}"`
 
-                if (!_.isEqual(newData, oldData)) {
-                    const transcript = await strapi
-                        .service('plugin::auto-content.fetchTranscriptService')
-                        .getTranscript(newData.URL, newData.StartTime, newData.EndTime)
+                if (data.Description) mdx += `\n>\n${data.Description}`
+                else mdx += '>'
 
-                    var mdx = '<YoutubeVideo\n' +
+                mdx += '\n</YoutubeVideo>'
+
+                event.params.data.CleanText = transcript;
+                event.params.data.MDX = mdx;
+            }
+        },
+        beforeUpdate: async (event) => {
+            // incoming data update
+            const { data, where } = event.params;
+
+            // omit fields that are not included in oldData
+            var newData = _.omit(data, ["__component", "__temp_key__", "updatedAt"]);
+            // console.log(newData);
+
+            // get the old data from the database
+            const oldData = await strapi
+                .query("page.video")
+                .findOne({ where: { id: where.id } });
+
+            // console.log(oldData);
+
+            if (!_.isEqual(newData, oldData)) {
+                const transcript = await strapi
+                    .service('plugin::auto-content.fetchTranscriptService')
+                    .getTranscript(newData.URL, newData.StartTime, newData.EndTime)
+
+                var mdx = '<YoutubeVideo\n' +
                     `src=${newData.URL}\n`
-                
-                    if (newData.Title) mdx += `title="${newData.Title}"`
-                
-                    if (newData.Description) mdx += `\n>\n${newData.Description}`                
-                    else mdx += '>'
 
-                    mdx += '\n</YoutubeVideo>'
+                if (newData.Title) mdx += `title="${newData.Title}"`
 
-                    event.params.data.CleanText = transcript;
-                    event.params.data.MDX = mdx;
-                }
-            },
+                if (newData.Description) mdx += `\n>\n${newData.Description}`
+                else mdx += '>'
+
+                mdx += '\n</YoutubeVideo>'
+
+                event.params.data.CleanText = transcript;
+                event.params.data.MDX = mdx;
+            }
         },
     });
 }
