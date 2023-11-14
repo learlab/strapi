90f241417875eb499f3757f6b698eb7b887c0337
theannachen <annachen031014@gmail.com>
2023-11-14 11:47:39 -0600

    update bash script

--- convert.sh
+++ convert.sh
@@ -1,43 +1,38 @@
 #!/bin/bash
 
-# Import required modules
-source ./fetch.sh
-source ./fs.sh
+# This script fetches data from a remote API and writes it to files
 
-# Declare variables
-data=""
-filename=""
+# Fetch data from the API
+data=$(curl -X GET "https://itell-strapi-um5h.onrender.com/api/pages?populate=*" --header "Cache-Control: no-store")
 
-# Fetch data from API
-res=$(fetch "https://itell-strapi-um5h.onrender.com/api/pages?populate=*" "{cache: \"no-store\"}")
-data=$(echo "$res" | jq -r '.data')
+# Loop through the data
+for ((i=0; i<$(echo $data | jq '.data | length'); i++)); do
+  curData=$(echo $data | jq ".data[$i].attributes")
+  chapterID=$(echo $curData | jq -r '.chapter.data.id')
 
-# Handle errors
-if [[ -n "$res" && "$res" != "null" ]]; then
-  echo "$res"
-  exit 1
-fi
+  # Fetch chapter data from the API
+  chapterData=$(curl -X GET "https://itell-strapi-um5h.onrender.com/api/chapters/$chapterID?populate=*" --header "Cache-Control: no-store")
+  chapterDataAttributes=$(echo $chapterData | jq '.data.attributes')
 
-# Loop through data
-for ((i=0; i<$(echo "$data" | jq -r '. | length'); i++)); do
-  curData=$(echo "$data" | jq -r '.['"$i"'].attributes')
-  filename="output/$(echo "$curData" | jq -r '.Title' | tr '[:upper:]' '[:lower:]' | tr -d '[:space:]').txt"
+  # Generate file path and filename
+  filePath=$(echo $chapterDataAttributes | jq -r '.module.data.attributes.Title' | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+  filePath+=$(echo $chapterDataAttributes | jq -r '.Title' | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+  filename="$filePath/$(echo $curData | jq -r '.Title' | tr '[:upper:]' '[:lower:]' | tr ' ' -).txt"
 
-  # Write to file
-  fs.writeFile "$filename" "---"
-  fs.writeFile "$filename" "$(echo "$curData" | jq -r '.Title')"
-  fs.writeFile "$filename" "---"
-
-  # Loop through content
-  for ((j=0; j<$(echo "$curData" | jq -r '.Content | length'); j++)); do
-    curContent=$(echo "$curData" | jq -r '.Content['"$j"']')
-
-    if [[ "$(echo "$curContent" | jq -r '.__component')" == "page.chunk" ]]; then
-      inputString="<div className=\"content-chunk\" data-subsection-id = \"$(echo "$curContent" | jq -r '.Header')\">"
-      fs.writeFile "$filename" "$inputString"
-      fs.writeFile "$filename" "$(echo "$curContent" | jq -r '.MDX')"
-      fs.writeFile "$filename" "</div>"
-    elif [[ "$(echo "$curContent" | jq -r '.__component')" == "page.video" ]]; then
+  # Write data to the file
+  echo "---" > $filename
+  echo $(echo $curData | jq -r '.Title') >> $filename
+  echo "---" >> $filename
+  
+  # Loop through the content
+  for ((j=0; j<$(echo $curData | jq '.Content | length'); j++)); do
+    content=$(echo $curData | jq ".Content[$j]")
+    if [[ $(echo $content | jq -r '.__component') == "page.chunk" ]]; then
+      inputString="<div className=\"content-chunk\" data-subsection-id = \"$(echo $content | jq -r '.Header')\">"
+      echo $inputString >> $filename
+      echo $(echo $content | jq -r '.MDX') >> $filename
+      echo "</div>" >> $filename
+    elif [[ $(echo $content | jq -r '.__component') == "page.video" ]]; then
       # Handle page.video component
       :
     fi
