2aada90ad4a609eb89debfbd980c79bda8b89a1b
Langdon <55119338+langdonholmes@users.noreply.github.com>
2023-12-18 23:01:50 +0000

    rework lifecycles

--- src/api/page/content-types/page/embeddings.js
+++ src/api/page/content-types/page/embeddings.js
@@ -1,22 +1,34 @@
-'use strict';
+"use strict";
 
 async function generatePageEmbeddings(ctx) {
-    const entry = await strapi.entityService.findOne('api::page.page', ctx.id, {populate: "*"});
-    const module = await strapi.entityService.findOne('api::chapter.chapter', entry.chapter.id, {populate: "module"});
+  const entry = await strapi.entityService.findOne("api::page.page", ctx.id, {
+    populate: "*",
+  });
 
-    const payload = entry.Content.map(item => ({
-        text_slug: entry.text.slug,
-        module_slug: module.module.slug,
-        chapter_slug: entry.chapter.slug,
-        page_slug: entry.slug,
-        chunk_slug: item.Slug,
-        content: item.CleanText,
-      }));
+  var chapter = null;
+  const chapter_id = entry.chapter ? entry.chapter.id : null;
+  if (chapter_id) {
+    chapter = await strapi.entityService.findOne(
+      "api::chapter.chapter",
+      chapter_id,
+      { populate: "module" }
+    );
+  }
 
-    payload.map(item => strapi.service('api::page.page').generateEmbedding(item));
+  const payload = entry.Content.map((item) => ({
+    text_slug: entry.text.slug,
+    module_slug: chapter.module ? chapter.module.slug : null,
+    chapter_slug: chapter ? chapter.slug : null,
+    page_slug: entry.slug,
+    chunk_slug: item.Slug,
+    content: item.CleanText,
+  }));
 
+  payload.map((item) =>
+    strapi.service("api::page.page").generateEmbedding(item)
+  );
 }
 
 module.exports = {
-    generatePageEmbeddings,
+  generatePageEmbeddings,
 };
--- src/plugins/auto-content/server/register.js
+++ src/plugins/auto-content/server/register.js
@@ -1,50 +1,51 @@
-'use strict';
+"use strict";
 
 module.exports = ({ strapi }) => {
-  strapi.customFields.register([{
-    name: 'question',
-    plugin: 'auto-content',
-    type: 'text',
-    inputSize: {
-      default: 12,
-      isResizable: true,
+  strapi.customFields.register([
+    {
+      name: "question",
+      plugin: "auto-content",
+      type: "text",
+      inputSize: {
+        default: 12,
+        isResizable: true,
+      },
     },
-  }, 
-  {
-    name: 'constructedResponse',
-    plugin: 'auto-content',
-    type: 'text',
-    inputSize: {
-      default: 12,
-      isResizable: true,
+    {
+      name: "constructedResponse",
+      plugin: "auto-content",
+      type: "text",
+      inputSize: {
+        default: 12,
+        isResizable: true,
+      },
     },
-  }, 
-  {
-    name: 'generatedQuestion',
-    plugin: 'auto-content',
-    type: 'text',
-    inputSize: {
-      default: 12,
-      isResizable: true,
+    {
+      name: "generatedQuestion",
+      plugin: "auto-content",
+      type: "text",
+      inputSize: {
+        default: 12,
+        isResizable: true,
+      },
     },
-  }, 
-  {
-    name: 'keyPhrase',
-    plugin: 'auto-content',
-    type: 'text',
-    inputSize: {
-      default: 12,
-      isResizable: true,
+    {
+      name: "keyPhrase",
+      plugin: "auto-content",
+      type: "text",
+      inputSize: {
+        default: 12,
+        isResizable: true,
+      },
     },
-  },
-  {
-    name: 'slug',
-    plugin: 'auto-content',
-    type: 'text',
-    inputSize: {
-      default: 12,
-      isResizable: true,
+    {
+      name: "slug",
+      plugin: "auto-content",
+      type: "text",
+      inputSize: {
+        default: 12,
+        isResizable: true,
+      },
     },
-  }
-]);
+  ]);
 };
