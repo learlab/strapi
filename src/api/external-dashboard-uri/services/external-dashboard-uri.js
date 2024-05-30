139853b9dfff55b5373c3c50db40db9fce02e956
Joon Suh Choi <57017670+jchoi92k@users.noreply.github.com>
2024-05-30 05:22:26 -0500

    Add external dashboard URI
    Simple key value pairs

---
+++ src/api/external-dashboard-uri/services/external-dashboard-uri.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * external-dashboard-uri service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::external-dashboard-uri.external-dashboard-uri');
