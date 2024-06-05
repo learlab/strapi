a9774186c1e1772546e6f2cebc114fadb4204646
theannachen <annachen031014@gmail.com>
2024-06-04 21:57:18 -0400

    Revert "Merge pull request #15 from learlab/burgess/api-keys-strapi"
    This reverts commit 9e874c89de5f2e0903c13794c2d9580d9c70c51a, reversing
    changes made to f6a2c25a8bc7570ebb50c44c7ab3d216717dd733.

--- src/api/api-key/controllers/api-key.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * api-key controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::api-key.api-key');
--- src/api/api-key/routes/api-key.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * api-key router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::api-key.api-key');
