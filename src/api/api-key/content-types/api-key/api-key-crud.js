e410e9a1049d2983c97703170649356ba5dc57b0
kyleburgess <kylie.m.burgess@vanderbilt.edu>
2024-06-06 18:44:42 -0400

    Revert "Revert "Merge pull request #15 from learlab/burgess/api-keys-strapi""
    This reverts commit a9774186c1e1772546e6f2cebc114fadb4204646.

---
+++ src/api/api-key/content-types/api-key/api-key-crud.js
@@ -0,0 +1,48 @@
+"use strict";
+
+async function createAPIKey(ctx) {
+  const entry = await strapi.entityService.findOne(
+    "api::api-key.api-key",
+    ctx.id,
+    {
+      populate: "*",
+    }
+  );
+
+  const payload = {
+    nickname: entry.nickname,
+    role: entry.role,
+  };
+
+  const api_key = await strapi
+    .service("api::api-key.api-key")
+    .generateAPIKey(payload);
+
+  await strapi.entityService.update("api::api-key.api-key", entry.id, {
+    data: {
+      api_key: api_key,
+    },
+  });
+}
+
+async function deleteAPIKey(id) {
+  const entry = await strapi.entityService.findOne(
+    "api::api-key.api-key",
+    id,
+    {
+      populate: "*",
+    });
+
+  const payload = {
+    api_key: entry.api_key,
+  };
+
+  await strapi
+    .service("api::api-key.api-key")
+    .deleteAPIKey(payload);
+}
+
+module.exports = {
+  createAPIKey,
+  deleteAPIKey,
+};
---
+++ src/api/api-key/controllers/api-key.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * api-key controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::api-key.api-key');
