b399b19bf74c9e2981855d02409db85606b065c3
langdonholmes <55119338+langdonholmes@users.noreply.github.com>
2024-08-15 14:03:28 +0000

    use placeholder for pageslug in REPLs

--- src/extensions/updateFields.js
+++ src/extensions/updateFields.js
@@ -49,33 +49,6 @@ async function generateSlugBeforeUpdate(event) {
 async function generateChunkFields(event) {
   const { data } = event.params;
 
-  let pageSlug;
-  let pageID;
-
-  // Database entry needs to exist before the parent page can be located
-  // Database will not exist on "beforeCreate"
-  if (event.action === "beforeUpdate") {
-    // Finds the page that contains the current component
-    // Uses the underlying knex connection to interact directly with the DB.
-    pageID = await strapi.db
-      .connection("pages_components")
-      .where({ component_id: data.id })
-      .pluck("entity_id")
-      .then((arr) => arr[0]); // entity_id is unique; only one value in this array
-  }
-  if (pageID) {
-    // Using the pageID, find the Slug of the parent page.
-    const page = await strapi.entityService.findOne("api::page.page", pageID, {
-      fields: ["Slug"],
-    });
-    pageSlug = page.Slug;
-  } else {
-    // TODO: Handle the case where the chunk is being created.
-    // Probably need to set a field on the chunk to store the page slug.
-    // Strapi does not make the page available in the Chunk's event object.
-    pageSlug = "placeholder-page-slug-until-chunk-is-updated";
-  }
-
   // Update the Fields
   const cleanText = await strapi
     .service("plugin::auto-content.cleanTextService")
@@ -83,7 +56,7 @@ async function generateChunkFields(event) {
 
   const mdx = await strapi
     .service("plugin::auto-content.mdxService")
-    .mdx(pageSlug, data.Text);
+    .mdx(data.Text);
 
   event.params.data.CleanText = cleanText;
   event.params.data.MDX = mdx;
